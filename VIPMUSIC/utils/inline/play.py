5c262548110f5eebd1a6555a932d5a838b3257c1
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-09-20 12:12:13 +0530

    Update play.py

--- VIPMUSIC/utils/inline/play.py
+++ VIPMUSIC/utils/inline/play.py
@@ -1,6 +1,7 @@
 import math
-from config import SUPPORT_CHAT, OWNER_USERNAME
-from pyrogram.types import InlineKeyboardButton
+
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 from VIPMUSIC.utils.formatters import time_to_seconds
 
 
