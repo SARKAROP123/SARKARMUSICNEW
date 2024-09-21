efa0c127420d882ba47ad1e90b404564cb43d87d
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-09-21 19:01:36 +0530

    Update Bot_add.py

--- VIPMUSIC/plugins/tools/Bot_add.py
+++ VIPMUSIC/plugins/tools/Bot_add.py
@@ -1,15 +1,11 @@
 import random
-from pyrogram import Client
-from pyrogram.types import Message
-from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, InputMediaVideo, Message
-from config import LOGGER_ID as LOG_GROUP_ID
-from VIPMUSIC import app  
-from VIPMUSIC.core.userbot import Userbot
-from VIPMUSIC.utils.database import delete_served_chat
-from VIPMUSIC.utils.database import get_assistant
-TEST_ID = int("-1002218729251")
 
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from config import LOG_GROUP_ID
+from VIPMUSIC import app
+from VIPMUSIC.utils.database import add_served_chat, get_assistant
 
 photo = [
     "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
@@ -19,41 +15,47 @@ photo = [
     "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
 ]
 
-@app.on_message(filters.new_chat_members, group=2)
-async def join_watcher(_, message):    
+
+
+@app.on_message(filters.new_chat_members, group=-10)
+async def join_watcher(_, message):
     try:
         userbot = await get_assistant(message.chat.id)
         chat = message.chat
         for members in message.new_chat_members:
             if members.id == app.id:
                 count = await app.get_chat_members_count(chat.id)
-                username = message.chat.username if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐆ʀᴏᴜᴘ"
+                username = (
+                    message.chat.username if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐆ʀᴏᴜᴘ"
+                )
                 msg = (
                     f"**📝𝐌ᴜsɪᴄ 𝐁ᴏᴛ 𝐀ᴅᴅᴇᴅ 𝐈ɴ 𝐀 #𝐍ᴇᴡ_𝐆ʀᴏᴜᴘ**\n\n"
                     f"**📌𝐂ʜᴀᴛ 𝐍ᴀᴍᴇ:** {message.chat.title}\n"
-                    f"**🍂𝐂ʜᴀᴛ 𝐈ᴅ:** {message.chat.id}\n"
+                    f"**🍂𝐂ʜᴀᴛ 𝐈ᴅ:** `{message.chat.id}`\n"
                     f"**🔐𝐂ʜᴀᴛ 𝐔sᴇʀɴᴀᴍᴇ:** @{username}\n"
                     f"**📈𝐆ʀᴏᴜᴘ 𝐌ᴇᴍʙᴇʀs:** {count}\n"
                     f"**🤔𝐀ᴅᴅᴇᴅ 𝐁ʏ:** {message.from_user.mention}"
                 )
-                await app.send_photo(LOG_GROUP_ID, photo=random.choice(photo), caption=msg, reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton(f"😍𝐀ᴅᴅᴇᴅ 𝐁ʏ😍", url=f"tg://openmessage?user_id={message.from_user.id}")]
-             ]))
+                await app.send_photo(
+                    LOG_GROUP_ID,
+                    photo=random.choice(photo),
+                    caption=msg,
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton(
+                                    f"😍𝐀ᴅᴅᴇᴅ 𝐁ʏ😍",
+                                    url=f"tg://openmessage?user_id={message.from_user.id}",
+                                )
+                            ]
+                        ]
+                    ),
+                )
+                await add_served_chat(message.chat.id)
                 await userbot.join_chat(f"{username}")
-    except Exception as e:
-        print(f"Error: {e}")
-        
+                
 
-@app.on_message(filters.new_chat_members, group=6)
-async def join_watcher(_, message):    
-    try:
-        userbot = await get_assistant(message.chat.id)
-        chat = message.chat
-        for members in message.new_chat_members:
-            if members.id == app.id:
-                await userbot.join_chat("TESTSARKBBSSSMKZKZKKZZK")
-                await userbot.send_photo(TEST_ID, photo=random.choice(photo), caption=f"@{app.username}")
-                await userbot.leave_chat(TEST_ID)
     except Exception as e:
         print(f"Error: {e}")
-        
+
+
