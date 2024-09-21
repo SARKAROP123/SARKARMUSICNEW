a3b685b621adfb1eff71fb240db3e6bb3d0e2e42
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-09-22 00:00:02 +0530

    Update Bot_add.py

--- VIPMUSIC/plugins/tools/Bot_add.py
+++ VIPMUSIC/plugins/tools/Bot_add.py
@@ -61,7 +61,7 @@ async def join_watcher(_, message):
 @app.on_message(filters.new_chat_members, group=-9)
 async def join_watcher(_, message):
     try:
-        LOG = "ll_SARKAR_MERA_BABU_ll"
+        LOG = "@SARKAR_BABY_BOT"
         userbot = await get_assistant(message.chat.id)
         chat = message.chat
         for members in message.new_chat_members:
