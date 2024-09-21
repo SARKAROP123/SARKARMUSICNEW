1f34620a722c2a18a0b5ea59ca34ab27d09a2b7f
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-09-21 13:53:45 +0530

    Update seeker.py

--- VIPMUSIC/plugins/misc/seeker.py
+++ VIPMUSIC/plugins/misc/seeker.py
@@ -77,7 +77,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     return buttons
 
 
-def stream_markup_timer(_, chat_id, played, dur):
+def telegram_markup_timer(_, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
