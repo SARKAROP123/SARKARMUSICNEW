289a88d941e9f6645ac222a69f83f11125793737
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-10-24 18:15:51 +0530

    Update __init__.py

--- VIPMUSIC/__init__.py
+++ VIPMUSIC/__init__.py
@@ -42,3 +42,14 @@ YOUTUBE = {
 }
 
 os.environ["TOKEN_DATA"] = json.dumps(YOUTUBE)
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+HELPABLE = {}
